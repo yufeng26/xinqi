34e8c3047bc2e09d5c0c6ccff7c3e08c9e100c7c
huangchunlong <huangchunlong@itangquan.com>
2020-10-20 00:11:03 +0800

    修改文字

--- src/views/Device/planreport.vue
+++ src/views/Device/planreport.vue
@@ -27,10 +27,10 @@
             出生年月：<span>{{ testresult.BirthDate }}</span>
           </td>
           <td>
-            测试时间：<span>{{ testresult.CreateTime }}</span>
+            训练时间：<span>{{ testresult.CreateTime }}</span>
           </td>
           <td>
-            测试时长：<span>{{ testresult.TestTime }}</span>
+            训练时长：<span>{{ testresult.TestTime }}</span>
           </td>
         </tr>
       </table>
@@ -153,8 +153,8 @@ export default {
         disimions: [],
         Suggestion: "",
         BrokenLine1: [],
-        BrokenLine2: []
-      }
+        BrokenLine2: [],
+      },
     };
   },
   methods: {
@@ -163,7 +163,7 @@ export default {
       let params = new URLSearchParams();
       params.append("id", this.testresult.ID);
       // 百思锐详情
-      this.$ReportOptionAPI.GetBaisiruiReport(params, function(data) {
+      this.$ReportOptionAPI.GetBaisiruiReport(params, function (data) {
         if (data.Code == 1) {
           v.testresult = data.Result;
           v.initChart();
@@ -185,36 +185,36 @@ export default {
       myChart1.setOption({
         color: ["#3cc5a3", "#ffc000", "#5cdbf2"],
         tooltip: {
-          trigger: "axis"
+          trigger: "axis",
         },
         legend: {
-          data: ["专注度", "放松度"]
+          data: ["专注度", "放松度"],
         },
         title: {
           text: "脑波曲线",
           textStyle: {
             left: "center",
-            fontSize: 14
+            fontSize: 14,
           },
           fontSize: 12,
           left: "center",
-          top: 15
+          top: 15,
         },
         tooltip: {
           show: true,
           trigger: "axis",
           axisPointer: {
             type: "shadow",
-            shadowStyle: "rgba(150,150,150,0.3)"
-          }
+            shadowStyle: "rgba(150,150,150,0.3)",
+          },
         },
         grid: [{ bottom: 40 }, { top: 50 }, { left: 30 }, { right: 30 }],
         xAxis: {
           type: "category",
-          data: xData
+          data: xData,
         },
         yAxis: {
-          type: "value"
+          type: "value",
         },
         series: [
           {
@@ -223,7 +223,7 @@ export default {
             stack: "总量",
             data: this.testresult.lstAtt
               ? JSON.parse(this.testresult.lstAtt)
-              : []
+              : [],
           },
           {
             name: "放松度",
@@ -231,9 +231,9 @@ export default {
             stack: "总量",
             data: this.testresult.lstMed
               ? JSON.parse(this.testresult.lstMed)
-              : []
-          }
-        ]
+              : [],
+          },
+        ],
       });
       // 绘制图表
       const data2 = this.testresult.Factorscores
@@ -245,37 +245,37 @@ export default {
           trigger: "axis",
           axisPointer: {
             // 坐标轴指示器，坐标轴触发有效
-            type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-          }
+            type: "shadow", // 默认为直线，可选为：'line' | 'shadow'
+          },
         },
         grid: {
           left: "3%",
           right: "4%",
           bottom: "3%",
-          containLabel: true
+          containLabel: true,
         },
         xAxis: [
           {
             type: "category",
-            data: data2.map(item => item.DimensionName),
+            data: data2.map((item) => item.DimensionName),
             axisTick: {
-              alignWithLabel: true
-            }
-          }
+              alignWithLabel: true,
+            },
+          },
         ],
         yAxis: [
           {
-            type: "value"
-          }
+            type: "value",
+          },
         ],
         series: [
           {
             name: "分数",
             type: "bar",
             barWidth: "60%",
-            data: data2.map(item => item.Score)
-          }
-        ]
+            data: data2.map((item) => item.Score),
+          },
+        ],
       });
     },
     //导出报告
@@ -289,14 +289,14 @@ export default {
       document.body.appendChild(elt);
       elt.click();
       document.body.removeChild(elt);
-    }
+    },
   },
   mounted() {
     // 获取路由参数，回去详情数据
     this.testresult.ID = this.$route.query.ID;
     this.getdetail();
   },
-  computed: {}
+  computed: {},
 };
 </script>
 
--- src/views/Device/pubreport.vue
+++ src/views/Device/pubreport.vue
@@ -27,10 +27,10 @@
             出生年月：<span>{{ testresult.BirthDate }}</span>
           </td>
           <td>
-            测试时间：<span>{{ testresult.CreateTime }}</span>
+            训练时长：<span>{{ testresult.CreateTime }}</span>
           </td>
           <td>
-            测试时长：<span>{{ testresult.TestTime }}</span>
+            训练时长：<span>{{ testresult.TestTime }}</span>
           </td>
         </tr>
       </table>
@@ -87,8 +87,8 @@ export default {
         Suggestion: "",
         planschemelist: "",
         BrokenLine1: [],
-        BrokenLine2: []
-      }
+        BrokenLine2: [],
+      },
     };
   },
   methods: {
@@ -96,7 +96,7 @@ export default {
       let v = this;
       let params = new URLSearchParams();
       params.append("Id", this.testresult.ID);
-      this.$TestResultAPI.getReportResult(params, function(data) {
+      this.$TestResultAPI.getReportResult(params, function (data) {
         if (data.Code == 1) {
           v.testresult = data.Result;
           v.initChart();
@@ -127,36 +127,36 @@ export default {
           text: "图表1",
           textStyle: {
             left: "center",
-            fontSize: 14
+            fontSize: 14,
           },
           fontSize: 12,
           left: "center",
-          top: 15
+          top: 15,
         },
         tooltip: {
           show: true,
           trigger: "axis",
           axisPointer: {
             type: "shadow",
-            shadowStyle: "rgba(150,150,150,0.3)"
-          }
+            shadowStyle: "rgba(150,150,150,0.3)",
+          },
         },
         grid: [{ bottom: 40 }, { top: 50 }, { left: 30 }, { right: 30 }],
         xAxis: {
           type: "category",
-          data: xData1
+          data: xData1,
         },
         yAxis: {
-          type: "value"
+          type: "value",
         },
         series: [
           {
             data: this.testresult.BrokenLine1
               ? JSON.parse(this.testresult.BrokenLine1)
               : [],
-            type: "line"
-          }
-        ]
+            type: "line",
+          },
+        ],
       });
       // 绘制图表
       myChart2.setOption({
@@ -165,36 +165,36 @@ export default {
           text: "图表1",
           textStyle: {
             left: "center",
-            fontSize: 14
+            fontSize: 14,
           },
           fontSize: 12,
           left: "center",
-          top: 15
+          top: 15,
         },
         tooltip: {
           show: true,
           trigger: "axis",
           axisPointer: {
             type: "shadow",
-            shadowStyle: "rgba(150,150,150,0.3)"
-          }
+            shadowStyle: "rgba(150,150,150,0.3)",
+          },
         },
         grid: [{ bottom: 40 }, { top: 50 }, { left: 30 }, { right: 30 }],
         xAxis: {
           type: "category",
-          data: xData2
+          data: xData2,
         },
         yAxis: {
-          type: "value"
+          type: "value",
         },
         series: [
           {
             data: this.testresult.BrokenLine2
               ? JSON.parse(this.testresult.BrokenLine2)
               : [],
-            type: "line"
-          }
-        ]
+            type: "line",
+          },
+        ],
       });
     },
     //导出报告
@@ -208,14 +208,14 @@ export default {
       document.body.appendChild(elt);
       elt.click();
       document.body.removeChild(elt);
-    }
+    },
   },
   mounted() {
     // 获取路由参数，回去详情数据
     this.testresult.ID = this.$route.query.ID;
     this.getdetail();
   },
-  computed: {}
+  computed: {},
 };
 </script>
 
