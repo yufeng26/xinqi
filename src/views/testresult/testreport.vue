348d786abe7a01ded8e77c7c2dcfdb89d56d6708
huangchunlong <huangchunlong@itangquan.com>
2020-10-20 10:54:10 +0800

    颜色值

--- src/views/testresult/testreport.vue
+++ src/views/testresult/testreport.vue
@@ -72,7 +72,7 @@
                       background: getScoreColor(
                         dimisionList[n].Name,
                         dimisionList[n].score
-                      ).bgcolor
+                      ).bgcolor,
                     }"
                     >{{
                       getScoreColor(dimisionList[n].Name, dimisionList[n].score)
@@ -90,7 +90,7 @@
                       background: getScoreColor(
                         dimisionList[n + 1].Name,
                         dimisionList[n + 1].score
-                      ).bgcolor
+                      ).bgcolor,
                     }"
                     >{{
                       getScoreColor(
@@ -168,7 +168,7 @@ export default {
       zongfen_name: "总分",
       zongfen_grade: "",
       dimisionList: [],
-      colorArr: ["#ED4C45", "#ED49E7", "#D4BF00", "#0070E5", "#3CC24C"],
+      colorArr: ["#3CC24C", "#0070E5", "#D4BF00", "#ED49E7", "#ED4C45"],
       testresult: {
         ID: "",
         UserName: "",
@@ -186,8 +186,8 @@ export default {
         Suggestion: "",
         planschemelist: "",
         BrokenLine1: [],
-        BrokenLine2: []
-      }
+        BrokenLine2: [],
+      },
     };
   },
   methods: {
@@ -204,7 +204,7 @@ export default {
       let v = this;
       let params = new URLSearchParams();
       params.append("id", this.testresult.ID);
-      this.$TestResultAPI.getResultDetail(params, function(data) {
+      this.$TestResultAPI.getResultDetail(params, function (data) {
         if (data.Code == 1) {
           v.testresult = data.Result;
           v.tableData = data.Result.TrainPlanList;
@@ -335,7 +335,7 @@ export default {
         20000,
         17500,
         19438,
-        18188
+        18188,
       ];
       // 用数据函数循环x轴坐标
       let xData = chartData.map((item, index) => index + 1);
@@ -347,34 +347,34 @@ export default {
           text: "测试数据",
           textStyle: {
             left: "center",
-            fontSize: 14
+            fontSize: 14,
           },
           fontSize: 12,
           left: "center",
-          top: 15
+          top: 15,
         },
         tooltip: {
           show: true,
           trigger: "axis",
           axisPointer: {
             type: "shadow",
-            shadowStyle: "rgba(150,150,150,0.3)"
-          }
+            shadowStyle: "rgba(150,150,150,0.3)",
+          },
         },
         grid: [{ bottom: 40 }, { top: 50 }, { left: 30 }, { right: 30 }],
         xAxis: {
           type: "category",
-          data: xData
+          data: xData,
         },
         yAxis: {
-          type: "value"
+          type: "value",
         },
         series: [
           {
             data: chartData,
-            type: "line"
-          }
-        ]
+            type: "line",
+          },
+        ],
       });
     },
     // 等级
@@ -384,127 +384,127 @@ export default {
         if (score >= 0 && score <= 4) {
           tempObj = {
             bgcolor: this.colorArr[0],
-            level: "学习焦虑较低"
+            level: "学习焦虑较低",
           };
         } else if (score > 4 && score <= 8) {
           tempObj = {
             bgcolor: this.colorArr[3],
-            level: "学习焦虑中等"
+            level: "学习焦虑中等",
           };
         } else if (score > 8 && score <= 16) {
           tempObj = {
             bgcolor: this.colorArr[4],
-            level: "学习焦虑较高"
+            level: "学习焦虑较高",
           };
         }
       } else if (propertyStr === "躯体化" || propertyStr === "身体症状") {
         if (score >= 0 && score <= 4) {
           tempObj = {
             bgcolor: this.colorArr[0],
-            level: "较低"
+            level: "较低",
           };
         } else if (score > 4 && score <= 8) {
           tempObj = {
             bgcolor: this.colorArr[3],
-            level: "中等"
+            level: "中等",
           };
         } else if (score > 8 && score <= 16) {
           tempObj = {
             bgcolor: this.colorArr[4],
-            level: "偏多"
+            level: "偏多",
           };
         }
       } else if (propertyStr === "环境适应" || propertyStr === "环境适应性") {
         if (score >= 0 && score <= 31) {
           tempObj = {
             bgcolor: this.colorArr[0],
-            level: "适应性很差"
+            level: "适应性很差",
           };
         } else if (score > 31 && score <= 61) {
           tempObj = {
             bgcolor: this.colorArr[1],
-            level: "适应性较差"
+            level: "适应性较差",
           };
         } else if (score > 61 && score <= 91) {
           tempObj = {
             bgcolor: this.colorArr[2],
-            level: "适应性一般"
+            level: "适应性一般",
           };
         } else if (score > 91 && score <= 121) {
           tempObj = {
             bgcolor: this.colorArr[3],
-            level: "适应性较强"
+            level: "适应性较强",
           };
         } else if (score > 121 && score <= 151) {
           tempObj = {
             bgcolor: this.colorArr[4],
-            level: "适应性很强"
+            level: "适应性很强",
           };
         }
       } else if (propertyStr === "社交焦虑") {
         if (score >= 0 && score <= 4) {
           tempObj = {
             bgcolor: this.colorArr[0],
-            level: "社交焦虑较低"
+            level: "社交焦虑较低",
           };
         } else if (score > 4 && score <= 8) {
           tempObj = {
             bgcolor: this.colorArr[3],
-            level: "社交焦虑中等"
+            level: "社交焦虑中等",
           };
         } else if (score > 8 && score <= 11) {
           tempObj = {
             bgcolor: this.colorArr[4],
-            level: "社交焦虑较高"
+            level: "社交焦虑较高",
           };
         }
       } else if (propertyStr === "抑郁") {
         if (score >= 20 && score <= 40) {
           tempObj = {
             bgcolor: this.colorArr[0],
-            level: "无抑郁症状"
+            level: "无抑郁症状",
           };
         } else if (score > 40 && score <= 48) {
           tempObj = {
             bgcolor: this.colorArr[2],
-            level: "中度至重度抑郁"
+            level: "中度至重度抑郁",
           };
         } else if (score > 48 && score <= 56) {
           tempObj = {
             bgcolor: this.colorArr[3],
-            level: "轻微或轻度抑郁"
+            level: "轻微或轻度抑郁",
           };
         } else if (score > 56 && score <= 81) {
           tempObj = {
             bgcolor: this.colorArr[4],
-            level: "重度抑郁"
+            level: "重度抑郁",
           };
         }
       } else if (propertyStr === "自卑感" || propertyStr === "自卑") {
         if (score >= 36 && score <= 73) {
           tempObj = {
             bgcolor: this.colorArr[0],
-            level: "自尊感过强"
+            level: "自尊感过强",
           };
         } else if (score > 73 && score <= 109) {
           tempObj = {
             bgcolor: this.colorArr[1],
-            level: "自尊感较强"
+            level: "自尊感较强",
           };
         } else if (score > 109 && score <= 145) {
           tempObj = {
             bgcolor: this.colorArr[2],
-            level: "自尊感一般"
+            level: "自尊感一般",
           };
         } else if (score > 145 && score <= 181) {
           tempObj = {
             bgcolor: this.colorArr[3],
-            level: "自卑感较强"
+            level: "自卑感较强",
           };
         } else if (score > 181 && score <= 253) {
           tempObj = {
             bgcolor: this.colorArr[4],
-            level: "自卑感过强"
+            level: "自卑感过强",
           };
         }
       }
@@ -522,7 +522,7 @@ export default {
       // document.body.appendChild(elt);
       // elt.click();
       // document.body.removeChild(elt);
-    }
+    },
   },
   mounted() {
     // 获取路由参数，回去详情数据
@@ -532,8 +532,8 @@ export default {
   },
   computed: {},
   components: {
-    RadialProgressBar
-  }
+    RadialProgressBar,
+  },
 };
 </script>
 
